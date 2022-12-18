fa0500fa4bc28cf2cad3bc0c48390773de18c1b2
devMarilia <devMarilia.frontend@gmail.com>
2022-12-18 17:36:34 -0300

    🐞 fix: clear console

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -1,25 +1,9 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { UserAuth } from '../../context/AuthContext';
+
 
 function SignIn() {
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-  const [error, setError] = React.useState('');
-
-  // const {createUser} = UserAuth()
-
-  const handleSubmit = async (e) => {
-    e.preventDefaut();
-    setError("")
-    try {
-      await createUserWithEmailAndPassword()
-    } catch (e) {
-      setError(e.message);
-      console.log(message);
-    }
-  }
 
   return (
     <div className='max-w-[700px] mx-auto my-16 p-4'>
@@ -34,11 +18,11 @@ function SignIn() {
     <form>
       <div className='flex flex-col py-2'>
         <label className='py-2 font-medium'>Email Address</label>
-        <input onChange={(e) => setEmail(e.target.value)} className='border p-3' type="email" />
+        <input  className='border p-3' type="email" />
       </div>
       <div className='flex flex-col py-2'>
         <label className='py-2 font-medium'>Password</label>
-        <input onChange={(e) => setPassword(e.target.value)} className='border p-3' type="password" />
+        <input className='border p-3' type="password" />
       </div>
       <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white" >Sign Up</button>
     </form>
