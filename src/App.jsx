b4e2238dedf30d63d72c10d1da705b8a384b94e9
devMarilia <devMarilia.frontend@gmail.com>
2022-12-18 19:33:07 -0300

    ✨ feat: Init authentication firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
-import { Route, Routes } from "react-router-dom"
-import SignIn from "./components/SignIn"
-import SignUp from "./components/SignUp"
-import Accout from "./components/Accout"
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Signin from './components/SignIn';
+import Signup from './components/SignUp';
+import Account from './components/Account';
 
 function App() {
-
-
   return (
-    <div >
-      <Routes>
-        <Route path="/" element={<SignIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="accout" element={<Accout/>} />
-      </Routes>
+    <div>
+      <h1 className='text-center text-3xl font-bold'>
+        Firebase Auth & Context
+      </h1>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route
+            path='/account'
+            element={
+              <ProtectedRoute>
+                <Account />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </AuthContextProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
